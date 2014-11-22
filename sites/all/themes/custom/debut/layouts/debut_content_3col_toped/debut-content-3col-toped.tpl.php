9d84bfed5718c2f00a86e4f4a9133359b6aae09c
Alexander Shumenko <ashumenko@adyax.com>
2014-11-22 14:48:36 +0200

    task #001-Starting: Translations added. Common module migrate started.

---
+++ sites/all/themes/custom/debut/layouts/debut_content_3col_toped/debut-content-3col-toped.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @file
+ * Template (front) for the debut-content-3col-stacked layout.
+ */
+?>
+
+<?php if (!empty($content['top'])) : ?>
+  <div class="underheader">
+    <?php print $content['top'];?>
+  </div>
+<?php endif; ?>
+
+<?php if (!empty($content['content_pre'])) : ?>
+  <section class="pre-content">
+    <?php print $content['content_pre'];?>
+  </section>
+<?php endif; ?>
+
+<div class="content cols-3 panel-3col">
+
+  <aside class="col-a col panel-col-first">
+    <?php print $content['content_left'];?>
+  </aside>
+
+  <div class="col-b col panel-col">
+    <div class="messages-target"></div>
+    <?php print $content['content_center'];?>
+  </div>
+
+  <aside class="col-c col panel-col-last">
+    <?php print $content['content_right'];?>
+  </aside>
+
+</div>
