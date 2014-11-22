578df02455acec89a2254ac9e859732afc3589f7
Alexander Shumenko <ashumenko@adyax.com>
2014-11-22 16:57:04 +0200

    task #001-Starting: Theme minor fix.

---
+++ sites/all/themes/custom/debut/js/debut-custom.js
@@ -0,0 +1,86 @@
+/**
+ * @file
+ * Custom JS.
+ */
+
+// jQuery sign.
+var $ = $ || jQuery;
+
+// Namespace.
+var debut_custom = debut_custom || {
+  _inited: false,
+  _windows: {},
+};
+
+/**
+ * Log function.
+ */
+debut_custom.log = function(text, data) {
+  if (typeof console == "object" && $.isFunction(console.log))  {
+    if (data) {
+      console.log(text, data);
+    }
+    else {
+      console.log(text);
+    }
+  }
+}
+
+/**
+ * Attach Ajax.
+ */
+debut_custom.attach_ajax = function ($context, settings) {
+
+  // Ajax links click.
+  $context.find('.debut-ajax-link').once('debut-ajax-link', function () {
+    $(this).click(function () {
+      var $this = $(this);
+      // If ajax is disabled we skip this.
+      if (!$this.hasClass('debut-ajax-disabled')) {
+        var url = $(this).attr('data-ajax-url');
+        var name = $(this).attr('data-ajax-name');
+        if (!name || name == '') {
+          name = url;
+        }
+        site_ajax.ajax_call(name, url, {});
+      }
+    });
+  });
+}
+
+/**
+ * Attach behavior.
+ */
+Drupal.behaviors.debut_custom = {
+  'attach' : function (context, settings) {
+    debut_custom.attach($(context), settings);
+  }
+};
+
+/**
+ * Attach behavior handler.
+ */
+debut_custom.attach = function ($context, settings) {
+
+  // Attach Ajax.
+  debut_custom.attach_ajax($context, settings);
+
+  // Init.
+  if (!debut_custom._inited) {
+    debut_custom.init();
+  }
+
+  // Move message to proper place.
+  var $messages = $('.messages-container').html();
+  $('.messages-target').append($messages);
+}
+
+/**
+ * Init function.
+ */
+debut_custom.init = function () {
+
+  // Mark init as complete.
+  debut_custom._inited = true;
+
+}
---
+++ sites/all/themes/custom/debut/templates/node/node--news--full.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * @file
+ * Template file.
+ */
+?>
+<div class="news node-item news-item">
+  <div class="news-title">
+      <?php print $_html['title']; ?>
+  </div>
+  <?php if (!empty($news_image)) : ?>
+    <div class="news-image-full">
+      <?php print $news_image; ?>
+    </div>
+  <?php endif; ?>
+  <div class="news-body">
+    <?php print $_html['text']; ?>
+  </div>
+  <div class="clear"></div>
+  <span class="date"><?php print $_html['news_date']; ?></span>
+</div>
+<div class="clear"></div>
