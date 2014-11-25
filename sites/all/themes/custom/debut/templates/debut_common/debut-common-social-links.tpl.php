3be6f592af6d9889c0fcc6d2ba852b7d33bb9446
Alexander Shumenko <ashumenko@adyax.com>
2014-11-25 21:42:26 +0200

    task #006-Social_links_block: Social links block added.

---
+++ sites/all/themes/custom/debut/templates/debut_common/debut-common-social-links.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ *
+ * Template.
+ */
+?>
+<div class="social">
+  <div class="facebook">
+    <?php print $_html['fblike']; ?>
+  </div>
+  <div class="facebook fixed">
+    <?php print $_html['vk']; ?>
+  </div>
+  <div class="facebook">
+    <?php print $_html['tweet']; ?>
+  </div>
+  <div class="facebook">
+    <?php print $_html['gplus']; ?>
+  </div>
+</div>
