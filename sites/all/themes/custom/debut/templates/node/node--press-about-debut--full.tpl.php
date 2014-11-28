3b7a9352a226ad9778d879efe80ba82836e4e9b2
Eugene Kulishov <kei@iphone.net.ua>
2014-11-28 16:33:13 +0200

    Node press_about_debut theming (full).

---
+++ sites/all/themes/custom/debut/templates/node/node--press-about-debut--full.tpl.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * @file
+ *   Template file.
+ */
+?>
+<div class="news node-item news-item press-item">
+  <div class="news-title press-title">
+    <h6><?php print $_html['title']; ?></h6>
+  </div>
+  <?php if (!empty($_html['press_link'])) : ?>
+    <div class="link-press">
+      <?php print $_html['press_link']; ?>
+    </div>
+    <?php if (!empty($_html['author'])) : ?>
+      <div class="author-press">
+        <?php print $_html['author']; ?>
+      </div>
+    <?php endif; ?>
+  <?php endif; ?>
+  <div class="news-body">
+    <?php print $_html['body']; ?>
+  </div>
+  <?php if (!empty($_html['press_link'])) : ?>
+    <div class="link-press">
+      <?php print $_html['press_link']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if (!empty($_html['author'])) : ?>
+    <div class="author-press">
+      <?php print $_html['author']; ?>
+    </div>
+  <?php endif; ?>
+  <div class="clear"></div>
+  <span class="date"><?php print $_html['press_date']; ?></span>
+</div>
+<div class="clear"></div>
