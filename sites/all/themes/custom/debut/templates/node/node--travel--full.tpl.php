764470f80df386d8e721ae957bef26314f6e6bfd
Eugene Kulishov <kei@iphone.net.ua>
2014-12-02 17:36:51 +0200

    task #017-Node_travel_theming: view mode created. Features add.

---
+++ sites/all/themes/custom/debut/templates/node/node--travel--full.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * @file
+ * Template file.
+ */
+?>
+<div class="travels-descrption travels-descrption-item-full">
+  <div class="travels-description-title">
+    <h1><?php print $_html['title']; ?></h1>
+  </div>
+</div>
+<div class="travels-descrption-items">
+  <ul class="listing">
+    <?php print $_html['content']; ?>
+  </ul>
+</div>
