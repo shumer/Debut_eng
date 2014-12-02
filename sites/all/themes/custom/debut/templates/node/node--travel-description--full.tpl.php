f95c2cd6a739b6c9e8ff858fc75c4081ace81ac0
Eugene Kulishov <kei@iphone.net.ua>
2014-12-02 14:45:37 +0200

    Node travel description theming. Full, search, list modes was added.

---
+++ sites/all/themes/custom/debut/templates/node/node--travel-description--full.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * @file
+ *   Template file.
+ */
+?>
+<div class="documents node-item documents-item-full">
+  <div class="documents-title">
+      <?php print $_html['title']; ?>
+  </div>
+  <div class="documents-body">
+    <?php print $_html['body']; ?>
+  </div>
+  <div class="documents-date">
+    <?php print $_html['date']; ?>
+  </div>
+</div>
