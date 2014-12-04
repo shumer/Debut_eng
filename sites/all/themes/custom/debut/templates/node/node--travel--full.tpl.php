11f5f59a644e35a64fa1fdf208a3b12c371ca39d
Eugene Kulishov <kei@iphone.net.ua>
2014-12-04 12:35:09 +0200

    Task #018-Node_travel_listing_page: travel listing page add, view travel list create, features update, themes fix.

--- sites/all/themes/custom/debut/templates/node/node--travel--full.tpl.php
+++ sites/all/themes/custom/debut/templates/node/node--travel--full.tpl.php
@@ -5,13 +5,11 @@
  * Template file.
  */
 ?>
-<div class="travels-descrption travels-descrption-item-full">
+<div class="travels-description travels-description-item-full">
   <div class="travels-description-title">
     <h1><?php print $_html['title']; ?></h1>
   </div>
 </div>
-<div class="travels-descrption-items">
-  <ul class="listing">
-    <?php print $_html['content']; ?>
-  </ul>
+<div class="travels-description-list">
+  <?php print $_html['content']; ?>
 </div>
