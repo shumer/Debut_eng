63009b781981aa21c654f526a731ce7c3a7a5f25
Eugene Kulishov <kei@iphone.net.ua>
2014-12-19 11:58:42 +0200

    task #039: Jury and Person frame view mode added.

---
+++ sites/all/themes/custom/debut/templates/node/node--debut-prize-person--frame.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @file
+ *   Template file.
+ */
+?>
+<figure class="pic">
+  <?php print $_html['image']; ?>
+  <figcaption>
+    <?php print $_html['name']; ?>
+  </figcaption>
+</figure>
+
