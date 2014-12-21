0d015a880a247a3d1ba35cb94c3763d875e4064a
Alexander Shumenko <ashumenko@adyax.com>
2014-12-21 20:37:59 +0200

    task #036-Contact_us: Minor fix

--- sites/all/themes/custom/debut/templates/debut_common/debut-common-years-list.tpl.php
+++ sites/all/themes/custom/debut/templates/debut_common/debut-common-years-list.tpl.php
@@ -6,6 +6,7 @@
  */
 ?>
 <div class="years-select-wrapper">
+  <span class="year-select-text"><?php print t('Change year: '); ?></span>
 <?php print drupal_render($_html['options']); ?>
 </div>
 <div class="clear"></div>
