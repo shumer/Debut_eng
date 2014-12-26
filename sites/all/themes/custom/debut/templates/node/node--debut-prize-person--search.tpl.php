1feed6851845233e6230830d02638ee311393369
Eugene Kulishov <kei@iphone.net.ua>
2014-12-26 16:52:40 +0200

    task #047-Node_prize_person_theming: ALPHA version. Will be edit.

---
+++ sites/all/themes/custom/debut/templates/node/node--debut-prize-person--search.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Template file.
+ */
+?>
+<div class="item">
+  <span class="image"><?php print $node->_html['image']; ?></span><br>
+  <span class="text"><?php print $node->_html['title']; ?></span>
+</div>
