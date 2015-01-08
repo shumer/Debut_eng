03316611bd682bb074a76d72bf13fe9847ef45ed
Alexander Shumenko <ashumenko@adyax.com>
2015-01-08 18:05:03 +0200

    task #051-Prize_persons_listing_fixes: jury block added

---
+++ sites/all/themes/custom/debut/templates/debut_common/debut-common-prize-jury.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Template.
+ */
+?>
+<div class="block block-prize-person">
+  <div class="block-content">
+    <h4 class="block-title">
+      <span class="top"></span>
+      <span class="logo"></span>
+      <span class="mid"><?php print $_html['title']; ?></span>
+      <span class="bottom"></span>
+    </h4>
+    <div class="block-content-wrapper"><?php print $_html['node']; ?></div>
+  </div>
+</div>
