d965b43284a1d97aff10edc008e83a5f4003e822
Alexander Shumenko <ashumenko@adyax.com>
2015-01-09 22:50:38 +0200

    task 051-Prize_persons_listing_fixes: Books on HP added.

--- sites/all/themes/custom/debut/templates/node/node--ibooks--home-page.tpl.php
+++ sites/all/themes/custom/debut/templates/node/node--ibooks--home-page.tpl.php
@@ -6,9 +6,9 @@
  */
 ?>
 <div class="compact-books-item">
-  <?php if (!empty($_html['image'])) : ?>
+  <?php if (!empty($_html['field_book_image'])) : ?>
     <div class="book-image">
-      <?php print $_html['title']; ?><?php print $_html['image']; ?>
+      <?php print $_html['title']; ?><?php print $_html['field_book_image']; ?>
     </div>
   <?php endif; ?>
 </div>
