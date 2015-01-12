eec0194fc68c73b33af61820ded1a5a94d6720d2
Eugene Kulishov <kei@iphone.net.ua>
2015-01-12 17:42:40 +0200

    task #048: work on the bugs. Fix different cct and templates.

--- sites/all/themes/custom/debut/templates/field_collection_item/field-collection-item--field-person-bibliography--full.tpl.php
+++ sites/all/themes/custom/debut/templates/field_collection_item/field-collection-item--field-person-bibliography--full.tpl.php
@@ -7,12 +7,12 @@
 ?>
 <h3><?php print $_html['title']; ?></h3>
 <?php print $_html['short']; ?>
-<?php if ($_html['hidden']) : ?>
+<?php if (!empty($_html['hidden'])) : ?>
   <div id="<?php print $_data['id']; ?>" class="hidden">
     <?php print $_html['hidden']; ?>
     <div class="clear"></div>
   </div>
-  <div id="<?php print $_data['id']; ?>_button" data-class="<?php print $_data['id']; ?>" class="next form-buttons form-submit js-expand-button">
+  <div id="<?php print $_data['id']; ?>_button" data-ref-id="<?php print $_data['id']; ?>" class="next form-buttons form-submit js-expand-button">
     <?php print t('onward'); ?>
   </div>
   <div class="clear"></div>
