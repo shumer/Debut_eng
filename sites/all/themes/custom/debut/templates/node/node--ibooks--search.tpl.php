0f55fcc507566020818c24e3de0608b6d6ccaf29
Eugene Kulishov <kei@iphone.net.ua>
2015-01-19 17:44:04 +0200

    task #058: tablet theme fixed. Search templates fixed.

--- sites/all/themes/custom/debut/templates/node/node--ibooks--search.tpl.php
+++ sites/all/themes/custom/debut/templates/node/node--ibooks--search.tpl.php
@@ -17,10 +17,12 @@
     </div>
     <?php print $_html['description']; ?>
   </div>
-  <div class="authors">
-    <h2><?php print t('Authors'); ?></h2>
-    <?php print $_html['authors']; ?>
-  </div>
+  <?php if (!empty($_html['authors'])) :?>
+    <div class="authors">
+      <h2><?php print t('Authors'); ?></h2>
+      <?php print $_html['authors']; ?>
+    </div>
+  <?php endif; ?>
   <div class="clear"></div>
   <div class="publication"><?php print $_html['publication']; ?></div>
   <div class="clear"></div>
