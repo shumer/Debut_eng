8f66dc441bfbc3d4e453ec9336de50d918c7297e
Alexander Shumenko <ashumenko@adyax.com>
2015-01-22 17:53:00 +0200

    taks #060-Contac_us_mobile: Minor fix

--- sites/all/themes/custom/debut_mobile/templates/debut_common/debut-common-contact-submitted.tpl.php
+++ sites/all/themes/custom/debut_mobile/templates/debut_common/debut-common-contact-submitted.tpl.php
@@ -4,16 +4,9 @@
  * Template.
  */
 ?>
-<div class="debut-popup">
-  <section class="popup">
-    <div class="inside">
-      <a href="javascript: void(0)" class="close"><?php print t('Close'); ?></a>
-      <header>
-        <h3><?php print t('Contact Us'); ?></h3>
-      </header>
-      <div class="content">
-        <h3><span class="topic comment-added"><?php print $_html['message']; ?></span></h3>
-      </div>
-    </div>
-  </section>
+<div class="block debut-contact-form">
+  <div class="form-header">
+    <h3><?php print t('Contact Us'); ?></h3>
+    <p><?php print $_html['message']; ?></p>
+  </div>
 </div>
