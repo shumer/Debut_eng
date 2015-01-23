0fb3a2a1fea03e31b8b3aa0210a0226a07843eac
Eugene Kulishov <kei@iphone.net.ua>
2015-01-23 13:30:36 +0200

    task #062-Mobile_node_speech: listing and full templates added.

---
+++ sites/all/themes/custom/debut_mobile/templates/node/node--nobel-speech--full.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * @file
+ *   Template file.
+ */
+?>
+<article class="block-nobel-speech full">
+  <div class="part part-a">
+    <div class="pic">
+      <div class="pic-inner">
+        <?php print $_html['image']; ?>
+      </div>
+      <!--span class="legende"><?php print $_html['image_legende']; ?></span-->
+    </div>
+    <?php if (!empty($_html['title'])): ?>
+      <h1><?php print $_html['title']; ?></h1>
+      <h2><?php print $_html['author']; ?></h2>
+    <?php else: ?>
+      <h1><?php print $_html['author']; ?></h1>
+    <?php endif; ?>
+  </div>
+  <h2 class="sub-title"><?php print $_html['press_date']; ?></h2>
+  <div class="news-body">
+    <?php print $_html['body']; ?>
+  </div>
+  <div class="clear"></div>
+  <div class="block-share">
+    <span><?php print t('Share article'); ?></span>
+  </div>
+  <?php print $_html['social_links']; ?>
+</article>
+<div class="clear-bottom"></div>
