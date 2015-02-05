c8e5b8df73aec0c7fdcea296393a2ec0b7b67ba2
Eugene Kulishov <kei@iphone.net.ua>
2015-02-05 17:14:40 +0200

    task #077-Mobile_jury_listing: beta version - not finished name listing.

---
+++ sites/all/themes/custom/debut_mobile/templates/debut_common/debut-common-jury-years-list.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ *
+ * Template.
+ */
+?>
+<div class="block-jury-years">
+  <div class="jury-years-wrapper">
+    <?php foreach ($years as $link): ?>
+      <?php print $link; ?>
+    <?php endforeach; ?>
+  </div>
+</div>
+<div class="clearfix"></div>
