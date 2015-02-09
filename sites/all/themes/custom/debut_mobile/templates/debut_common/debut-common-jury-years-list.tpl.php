8dff7d49406c235b9f9566ec081580768f84c547
Eugene Kulishov <kei@iphone.net.ua>
2015-02-09 17:42:49 +0200

    task #077: slide for letters and years added. Minor css fix.

--- sites/all/themes/custom/debut_mobile/templates/debut_common/debut-common-jury-years-list.tpl.php
+++ sites/all/themes/custom/debut_mobile/templates/debut_common/debut-common-jury-years-list.tpl.php
@@ -7,9 +7,11 @@
 ?>
 <div class="block-jury-years">
   <div class="jury-years-wrapper">
-    <?php foreach ($years as $link): ?>
-      <?php print $link; ?>
-    <?php endforeach; ?>
+    <div class="years-slides">
+      <?php foreach ($years as $link): ?>
+        <?php print $link; ?>
+      <?php endforeach; ?>
+    </div>
   </div>
 </div>
 <div class="clearfix"></div>
