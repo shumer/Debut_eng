36fc8cd26f31a3071b774c6dd2801dec3a5aeb14
Eugene Kulishov <kei@iphone.net.ua>
2015-02-13 13:28:44 +0200

    task #077: "share page" block added.

---
+++ sites/all/themes/custom/debut_mobile/templates/views/debut_view_jury_prize_list/views-view-unformatted--debut-view-jury-prize-list--listing-view-mode.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file views-view-unformatted.tpl.php
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php print $row; ?>
+  </div>
+<?php endforeach; ?>
+  <div class="block-share">
+    <span><?php print t('Share page'); ?></span>
+  </div>
+<?php print $_html['social_links']; ?>
