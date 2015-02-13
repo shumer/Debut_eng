c87767b831f32d0a641adc74f4880393efb81679
Eugene Kulishov <kei@iphone.net.ua>
2015-02-13 12:45:44 +0200

    task #077: final minor fix.

--- sites/all/themes/custom/debut_mobile/templates/debut_common/debut-common-alphabet-header.tpl.php
+++ sites/all/themes/custom/debut_mobile/templates/debut_common/debut-common-alphabet-header.tpl.php
@@ -7,7 +7,7 @@
 <section class="block-alphabet block-persons">
   <div class="item">
     <div class="separator"></div>
-    <div class="alphabet alphabet-wrapper">
+    <div class="alphabet alphabet-wrapper slider-wrapper">
       <?php print $_html['alphabet']; ?>
     </div>
   </div>
