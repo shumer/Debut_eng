1bf17e633fc10fd80240d749273d7ee2c8a99fcd
Alexander Shumenko <ashumenko@adyax.com>
2015-02-18 15:08:30 +0200

    task #079-Mobile_search: Minor fix

--- sites/all/themes/custom/debut_mobile/templates/node/node--debut-jury--search.tpl.php
+++ sites/all/themes/custom/debut_mobile/templates/node/node--debut-jury--search.tpl.php
@@ -4,7 +4,7 @@
  * Template file.
  */
 ?>
-<div class="item">
-  <span class="image"><?php print $_html['image']; ?></span><br>
+<div class="item person-search-page">
+  <span class="image"><?php print $_html['image']; ?></span><br />
   <span class="text"><?php print $_html['title']; ?></span>
 </div>
--- sites/all/themes/custom/debut_mobile/templates/node/node--debut-prize-person--search.tpl.php
+++ sites/all/themes/custom/debut_mobile/templates/node/node--debut-prize-person--search.tpl.php
@@ -5,6 +5,6 @@
  */
 ?>
 <div class="item person-search-page">
-  <span class="image"><?php print $_html['image']; ?></span><br>
+  <span class="image"><?php print $_html['image']; ?></span><br />
   <span class="text"><?php print $_html['title']; ?></span>
 </div>
