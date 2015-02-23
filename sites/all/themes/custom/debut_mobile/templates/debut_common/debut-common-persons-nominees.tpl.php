695d65fdc449a2f737edac6327af74ad958fb409
Eugene Kulishov <kei@iphone.net.ua>
2015-02-23 15:53:57 +0200

    task #080: start work: basic css added, page updated.

---
+++ sites/all/themes/custom/debut_mobile/templates/debut_common/debut-common-persons-nominees.tpl.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @file
+ * Template placeholder.
+ */
+?>
+<section class="block block-article block-questions block-lexique persons-list">
+  <?php foreach ($_data['prizes'] as $rows) : ?>
+    <div class="prize-persons">
+      <p class="winners-links">
+        <?php foreach ($rows['captions'] as $caption) : ?>
+          <span class="winners <?php print $caption['class']; ?>"
+            <?php if (!empty($caption['ref-id'])) : ?>
+                data-ref-id="<?php print $caption['ref-id']; ?>">
+            <?php else :?>
+                onclick="debut_custom.hide_all_prizes($(this));">
+            <?php endif; ?>
+            <?php print $caption['title']; ?></span>
+        <?php endforeach; ?>
+      </p>
+    <?php foreach ($rows['data'] as $block) : ?>
+      <article class="<?php print $block['class']; ?>" id="<?php print $block['id']; ?>">
+          <?php if (!empty($block['title'])) : ?>
+            <h3><?php print $block['title']; ?></h3>
+          <?php endif; ?>
+        <?php print $block['html']; ?>
+      </article>
+    <?php endforeach; ?>
+    </div>
+  <?php endforeach; ?>
+
+  <div class="view-more-target"></div>
+
+  <?php if ($_data['show_more']) : ?>
+    <span class="text-throbber">
+		  <span class="button button-c">
+			  <a href="javascript: void(0)" data-ajax-url="<?php print $_data['show_more_url']; ?>" class="persons-view-more-button">
+			    <span>
+				    <?php print t('Show more'); ?>
+				  </span>
+		    </a>
+		  </span>
+    </span>
+  <?php endif; ?>
+</section>
