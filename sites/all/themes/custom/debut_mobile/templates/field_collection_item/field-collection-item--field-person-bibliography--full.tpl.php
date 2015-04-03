65a2176edb2cfb279d144ebeb6af9c2c3c2f7ee1
Eugene Kulishov <kei@iphone.net.ua>
2015-04-03 15:24:04 +0300

    Mobile filed_collection_full fix.

--- sites/all/themes/custom/debut_mobile/templates/field_collection_item/field-collection-item--field-person-bibliography--full.tpl.php
+++ sites/all/themes/custom/debut_mobile/templates/field_collection_item/field-collection-item--field-person-bibliography--full.tpl.php
@@ -5,16 +5,18 @@
  *   Template file.
  */
 ?>
-<div class="bibliography-item">
-  <h3 class="sub-title"><?php print $_html['title']; ?></h3>
-  <?php print $_html['short']; ?>
-  <?php if (!empty($_html['hidden'])) : ?>
-    <div class="block-button-more" data-target="bibliography-long-<?php print $_data['id']; ?>">
-      <span><?php print t('onward'); ?></span>
-    </div>
-    <div class="bibliography-long bibliography-long-<?php print $_data['id']; ?>" style="display: none">
-      <?php print $_html['hidden']; ?>
-    </div>
-    <div class="clearfix"></div>
-  <?php endif; ?>
-</div>
+<?php if (!empty($_html['title'])) : ?>
+  <div class="bibliography-item">
+    <h3 class="sub-title"><?php print $_html['title']; ?></h3>
+    <?php print $_html['short']; ?>
+    <?php if (!empty($_html['hidden'])) : ?>
+      <div class="block-button-more" data-target="bibliography-long-<?php print $_data['id']; ?>">
+        <span><?php print t('onward'); ?></span>
+      </div>
+      <div class="bibliography-long bibliography-long-<?php print $_data['id']; ?>" style="display: none">
+        <?php print $_html['hidden']; ?>
+      </div>
+      <div class="clearfix"></div>
+    <?php endif; ?>
+  </div>
+<?php endif; ?>
